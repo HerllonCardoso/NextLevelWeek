ccb4db12ecdbe9b82a7dbb2553558712b5a58443
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-06-04 15:47:39 -0300

    aula 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -32,11 +32,17 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value> Selecione a cidade </option>"
+    citySelect.disabled = true
+
     fetch(url)
         .then(res => res.json())
         .then(cities => {
+
+
+
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false
@@ -50,3 +56,49 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+//itens de coleta
+//pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+
+    const itemLi = event.target
+
+    //add or remove class
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = (itemLi.dataset.id)
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId  //true or false 
+        return itemFound
+    })
+
+
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else {
+
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+
+}
